36cec6401beabc3da1df34c5bda8de1cde1cca98
obsiidienne <140954202+obsiidienne@users.noreply.github.com>
2023-09-16 18:09:18 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin:"http://localhost:3000",
+    origin:"https://educationchatapp.vercel.app/auth",
     
   },
 });
