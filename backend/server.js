16e8cc9fdbb62fc2389cd3ab22ce20e837d9e09d
obsiidienne <140954202+obsiidienne@users.noreply.github.com>
2023-09-16 21:09:33 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin:"https://education-apzv.onrender.com/auth",
+    origin:"https://education-iasa.onrender.com",
     
   },
 });
