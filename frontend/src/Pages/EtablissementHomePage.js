e5b1f8c2a004e33b826cc3302e3dd003e2c6ab2e
obsiidienne <140954202+obsiidienne@users.noreply.github.com>
2023-09-25 13:24:52 +0100

    Update EtablissementHomePage.js

--- frontend/src/Pages/EtablissementHomePage.js
+++ frontend/src/Pages/EtablissementHomePage.js
@@ -16,27 +16,30 @@ const EtablissementHomePage = () => {
     <>
       <AdminNav />
       <EtabHome/>
-      <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial', marginTop:"-26%" }}>
-      <CDBSidebar textColor="#fff" backgroundColor="linear-gradient(45deg, #49a09d, #5f2c82)">
-        <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
-          <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
+      <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial', marginTop:"-50.2%"  }}>
+      <CDBSidebar textColor="#fff" backgroundColor="linear-gradient(to top, #accbee 0%, #e7f0fd 100%)">
+        <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"style={{ color: 'black' }}></i>}>
+          <a href="/" className="text-decoration-none" style={{ color: 'black' }}>
             Sidebar
           </a>
         </CDBSidebarHeader>
 
         <CDBSidebarContent className="sidebar-content">
           <CDBSidebarMenu>
-            <NavLink exact to="/Auth" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="columns">Ajouter un Utilisateur</CDBSidebarMenuItem>
+            <NavLink exact to="/Auth" activeClassName="activeClicked"style={{ color: 'black' }}>
+              <CDBSidebarMenuItem icon="user">Ajouter un Utilisateur</CDBSidebarMenuItem>
             </NavLink>
-            <NavLink exact to="/admin/list" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="table">Etablissement</CDBSidebarMenuItem>
+            <NavLink exact to="/admin/list" activeClassName="activeClicked"style={{ color: 'black' }}>
+              <CDBSidebarMenuItem icon="city">Etablissement</CDBSidebarMenuItem>
             </NavLink>
-            <NavLink exact to="/Admin/home" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="table">Utilisateurs</CDBSidebarMenuItem>
+            <NavLink exact to="/Admin/home" activeClassName="activeClicked"style={{ color: 'black' }}>
+              <CDBSidebarMenuItem icon="users">Utilisateurs</CDBSidebarMenuItem>
             </NavLink>
-            <NavLink exact to="/Admin/add" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="user">Ajouter un Etablissement</CDBSidebarMenuItem>
+            <NavLink exact to="/Admin/add" activeClassName="activeClicked"style={{ color: 'black' }}>
+              <CDBSidebarMenuItem icon="building">Ajouter un Etablissement</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/Admin/add" activeClassName="activeClicked"style={{ color: 'black' }}>
+              <CDBSidebarMenuItem icon="message">Archives</CDBSidebarMenuItem>
             </NavLink>
           </CDBSidebarMenu>
         </CDBSidebarContent>
