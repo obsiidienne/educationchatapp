e224baf8a2224e356f149d87e28574fc57ea54e6
obsiidienne <140954202+obsiidienne@users.noreply.github.com>
2023-10-05 22:10:35 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect("mongodb+srv://chaima:12345678sh@cluster0.2c9ulnf.mongodb.net/education?retryWrites=true&w=majority", {
+    const conn = await mongoose.connect("mongodb+srv://education:TaOTTD33htoI5cum@cluster0.ncsflbg.mongodb.net/?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
