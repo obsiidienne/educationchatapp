ad14191d6de7bc81b7e6656ad3253c9ce2f0e1ae
obsiidienne <140954202+obsiidienne@users.noreply.github.com>
2023-10-10 00:13:03 +0100

    Update adminRouter.js

--- backend/routes/adminRouter.js
+++ backend/routes/adminRouter.js
@@ -5,12 +5,6 @@ const {
   editUser,
   addUser,
 } = require("../controllers/adminController");
-const {
-  allEtablissement,
-  deleteEtablissement,
-  editEtablissement,
-  addEtablissement,
-} = require("../controllers/etablissementController");
 const { verifyAdmin } = require("../middleware/authAdmin");
 
 const router = require("express").Router();
@@ -20,9 +14,4 @@ router.get("/userlist",allUsers);
 router.delete("/delete-user/:userId", deleteUser);
 router.put("/edit-user", editUser);
 router.post("/add-user", addUser);
-router.get("/etablissementlist", allEtablissement);
-router.delete("/delete/:Id", deleteEtablissement);
-router.put("/edit", editEtablissement);
-router.post("/add", addEtablissement);
-
 module.exports = router;
