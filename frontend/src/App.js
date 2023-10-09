250f49a267520fa9e55c9a3d43a43a7fe4c6567d
obsiidienne <140954202+obsiidienne@users.noreply.github.com>
2023-10-10 00:15:37 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,9 +7,7 @@ import ChatPage from "./Pages/ChatPage";
 import { useEffect, useState } from "react";
 import AdminLoginPage from "./Pages/AdminLoginPage";
 import AdminHomePage from "./Pages/AdminHomePage";
-import EtablissementHomePage from "./Pages/EtablissementHomePage";
 import AddUserPage from "./Pages/AddUserPage";
-import AddEtablissementPage from "./Pages/AddEtablissementPage";
 import EditUserPage from "./Pages/EditUserPage";
 import Sidebar from "./Pages/sidebar";
 import Sidebarr from "./Components/sidebar/sidebar";
@@ -28,8 +26,6 @@ function App() {
         <Route path="auth/" element={<Auth />}></Route>
         <Route path="chats" element={<ChatPage />}></Route>
         <Route path="/admin" element={<AdminLoginPage />} />
-        <Route path="/admin/list" element={<EtablissementHomePage/>} />
-        <Route path="/admin/add" element={<AddEtablissementPage/>} />
         <Route path="/admin/home" element={<AdminHomePage />} />
         <Route path="/admin/add-user" element={<AddUserPage />} />
         <Route path="/admin/edit-user" element={<EditUserPage />} />    
