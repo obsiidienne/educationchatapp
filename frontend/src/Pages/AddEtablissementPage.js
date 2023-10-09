08acb7a61f4d833e717db558b9735433bd3173ec
obsiidienne <140954202+obsiidienne@users.noreply.github.com>
2023-10-10 00:14:20 +0100

    Delete frontend/src/Pages/AddEtablissementPage.js

--- frontend/src/Pages/AddEtablissementPage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AddEtablissement from "../Components/AddEtablissement/AddEtablissement";
-
-const AddEtablissementPage = () => {
-  return <AddEtablissement />;
-};
-
-export default AddEtablissementPage;
